3ecd69eaf33fd4bf435a488dbe436706c3f08a2b
giovanny <gmc.yanez@gmail.com>
2018-02-09 13:55:29 +0100

    Realizando cambios para que en la vista de Account el usuario pueda ver el historial de todas sus compras realizadas

--- proy_navid/ProyectoGio/module/myAccount/view/create_yourOrders.php
+++ proy_navid/ProyectoGio/module/myAccount/view/create_yourOrders.php
@@ -1,12 +1,79 @@
 <script type="text/javascript" src="module/myAccount/view/js/yourOrders.js"></script>
 
-<div class="container">
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<div id="TusPedidos">
-		xxxxxxxxxxxx
+<div class="container" id="TusPedidos">
+	<div class="panel panel-default aboutus-text">
+	  
+		<div class="panel-heading">
+			<div class=" row" >
+				<div class="col-md-4">
+					<b>Price: </b><p>EUR </p>
+				</div>
+				<div class="col-md-4">
+					<b>Total Price: </b><p>EUR </p>
+				</div>
+				<div class="col-md-4">
+					<b>Pedido: </b><p>EUR </p>
+				</div>										
+			</div>    
+		</div>
+
+		<div class="panel-body">
+		    <div class="row ">
+					
+					<div class="col-md-12">
+						<div class="col-md-9">
+						 <!-- ........................Para cada producto ...............................................-->
+							<div class="row">
+								<div class="col-md-3 " >
+									<a href="#" class="thumbnail">
+										<img src="media/default-avatar.png">
+									</a>
+								</div>
+
+								<div class="col-md-9  ">
+									<div class="">
+										<span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iste necessitatibus quos voluptatem ad quo numquam eveniet amet nisi, delectus. </span>
+									</div>
+									<br>
+									<div>
+										<button type="button" class="btn btn-primary" >Buy Again</button><br><br><br><br>		
+									</div>	
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-md-3 " >
+									<a href="#" class="thumbnail">
+										<img src="media/default-avatar.png"  > <!-- text-align:left; -->
+									</a>
+								</div>
+
+								<div class="col-md-9">
+									<div class="">
+										<span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit iste necessitatibus quos voluptatem ad quo numquam eveniet amet nisi, delectus. </span>
+									</div>
+									<br>
+									<div>
+										<button type="button" class="btn btn-primary" >Buy Again</button><br><br>		
+									</div>	
+								</div>
+							</div>
+						<!-- ........................Para cada producto ...............................................-->
+						</div>
+
+						<div class="col-md-3 " style=" text-align:center; ">					
+							<div class="list-group">
+								  <a href="#" class="list-group-item ">Cras justo odio</a>
+								  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+								  <a href="#" class="list-group-item">Morbi leo risus</a>
+								  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+								  <a href="#" class="list-group-item">Vestibulum at eros</a>
+							</div>
+						</div>
+
+					</div>
+				
+			</div>
+	 	</div>
 	</div>
 </div>
--- proy_navid/ProyectoGio/module/myAccount/view/js/yourOrders.js
+++ proy_navid/ProyectoGio/module/myAccount/view/js/yourOrders.js
@@ -41,9 +41,111 @@ $(document).ready(function () {
         $.post('module/myAccount/controller/controller_myAccount.php?type=pedidos',
                     {"usuarioLogeado":  usuarioLogeado},
         function(response){
-            console.log(response);
-             var json_cont2 = JSON.parse(response);
-             console.log(json_cont2);
+            console.log(response.length);
+             var json = JSON.parse(response);
+             console.log(json[0][0]['id_pedido']);
+             console.log(json[2].length);
+             console.log(json);
+
+
+
+
+            var div_prin =document.getElementById("TusPedidos"); 
+
+
+            /*-----------------------------------------------------------------------------*/  
+            var google = crearElementos("a",{"href":"http://google.com", "id":"prueba"},"google"),
+                youtube = crearElementos("a",{"href":"http://youtube.com"},"youtube"),
+                facebook = crearElementos("a",{"href":"http://facebook.com"},"facebook"),
+                links_conteiner = crearElementos("div",{"id":"links"},[google,youtube,facebook]);
+
+                div_prin.appendChild(links_conteiner); 
+            /*-----------------------------------------------------------------------------*/
+
+
+
+            for (var i = 0; i <json.length; i++) {
+                var div_panel =crearElementos("div", {"class":"panel panel-default aboutus-text"}),                    
+                    div_panel_heading=crearElementos("div", {"class": "panel-heading"}),
+                    div_panel_heading_row=crearElementos("div", {"class": "row"}),
+                    div_panel_heading_row_precio=crearElementos("div", {"class": "col-md-4"}),
+                    div_panel_heading_row_precioTotal=crearElementos("div", {"class": "col-md-4"}),
+                    div_panel_heading_row_numeroPedido=crearElementos("div", {"class": "col-md-4"}),
+                    p_precio=crearElementos("p",{"id":"precio"}, "EUR"),
+                    p_precioTotal=crearElementos("p",{"id":"precioTotal"}, "EUR"),
+                    p_numeroPedido=crearElementos("p",{"id":"numeroPedido"}, "Numero Pedido")
+                    strong_precio=crearElementos("strong",{"id":"precio"}, "Order Date"),
+                    strong_precioTotal=crearElementos("strong",{"id":"precio"}, "Total Price"),
+                    strong_numeroPedido=crearElementos("strong",{"id":"precio"}, "Order Number"),
+
+                    divPanelBody=crearElementos("div", {"class":"panel-body"}),
+                    divPanelBody_row=crearElementos("div", {"class":"row"}),
+                    divPanelBody_row_colmd12=crearElementos("div", {"class":"col-md-12"}),
+                    divPanelBody_row_colmd12_colmd9=crearElementos("div", {"class":"col-md-9"}),
+                    divPanelBody_row_colmd12_colmd3=crearElementos("div", {"class":"col-md-3"}),
+                    divPanelBody_row_colmd12_colmd3_div=crearElementos("div", {"class":"list-group", "style":"text-align:center;"}),
+                    divPanelBody_row_colmd12_colmd3_div_a1=crearElementos("a", {"class":"list-group-item"}, "Lorem ipsum dolor sit."),
+                    divPanelBody_row_colmd12_colmd3_div_a2=crearElementos("a", {"class":"list-group-item"}, "Lorem ipsum dolor sit."),
+                    divPanelBody_row_colmd12_colmd3_div_a3=crearElementos("a", {"class":"list-group-item"}, "Lorem ipsum dolor sit."),
+                    divPanelBody_row_colmd12_colmd3_div_a4=crearElementos("a", {"class":"list-group-item"}, "Lorem ipsum dolor sit."),
+                    divPanelBody_row_colmd12_colmd3_div_a5=crearElementos("a", {"class":"list-group-item"}, "Lorem ipsum dolor sit.");
+                
+                // div_prin.appendChild(div_panel); 
+                div_prin.appendChild(div_panel);                 
+                div_panel.appendChild(div_panel_heading);     
+                div_panel_heading.appendChild(div_panel_heading_row); 
+                div_panel_heading_row.appendChild(div_panel_heading_row_precio);
+                div_panel_heading_row.appendChild(div_panel_heading_row_precioTotal);
+                div_panel_heading_row.appendChild(div_panel_heading_row_numeroPedido);
+                div_panel_heading_row_precio.appendChild(strong_precio);  
+                div_panel_heading_row_precio.appendChild(p_precio); 
+                div_panel_heading_row_precioTotal.appendChild(strong_precioTotal);
+                div_panel_heading_row_precioTotal.appendChild(p_precioTotal);
+                div_panel_heading_row_numeroPedido.appendChild(strong_numeroPedido);
+                div_panel_heading_row_numeroPedido.appendChild(p_numeroPedido);
+
+                div_panel.appendChild(divPanelBody);
+                
+                divPanelBody.appendChild(divPanelBody_row);
+                divPanelBody_row.appendChild(divPanelBody_row_colmd12);
+                divPanelBody_row_colmd12.appendChild(divPanelBody_row_colmd12_colmd9);
+                divPanelBody_row_colmd12.appendChild(divPanelBody_row_colmd12_colmd3);
+                divPanelBody_row_colmd12_colmd3.appendChild(divPanelBody_row_colmd12_colmd3_div);
+                divPanelBody_row_colmd12_colmd3_div.appendChild(divPanelBody_row_colmd12_colmd3_div_a1);
+                divPanelBody_row_colmd12_colmd3_div.appendChild(divPanelBody_row_colmd12_colmd3_div_a2);
+                divPanelBody_row_colmd12_colmd3_div.appendChild(divPanelBody_row_colmd12_colmd3_div_a3);
+                divPanelBody_row_colmd12_colmd3_div.appendChild(divPanelBody_row_colmd12_colmd3_div_a4);
+
+                    console.log(json[i].length);
+                    
+                for (var j = 0; j <json[i].length; j++) {
+                    console.log(json[i].length);
+                    var divPanelBody_row_colmd12_colmd9_row=crearElementos("div", {"class":"row"}),
+                        divPanelBody_row_colmd12_colmd9_row_colMd3=crearElementos("div", {"class":"col-md-3"}),
+                        divPanelBody_row_colmd12_colmd9_row_colMd3_a=crearElementos("a", {"class":"thumbnail"}),
+                        divPanelBody_row_colmd12_colmd9_row_colMd3_a_img=crearElementos("img", {"src":"#"}),
+
+                        divPanelBody_row_colmd12_colmd9_row_colMd9=crearElementos("div", {"class":"col-md-9"}),
+                        divPanelBody_row_colmd12_colmd9_row_colMd9_div=crearElementos("div"),
+                        divPanelBody_row_colmd12_colmd9_row_colMd9_div_span=crearElementos("span", {"id":"9"}, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam hic illo praesentium alias, dicta neque."),
+                        divPanelBody_row_colmd12_colmd9_row_colMd9_br=crearElementos("br"),
+                        divPanelBody_row_colmd12_colmd9_row_colMd9_div2=crearElementos("div"),
+                        divPanelBody_row_colmd12_colmd9_row_colMd9_div2_button=crearElementos("button", {"class":"btn btn-primary"}, "By Again");
+                
+                    p_precio.innerHTML=json[i][j]['order_date'];
+                    divPanelBody_row_colmd12_colmd9.appendChild(divPanelBody_row_colmd12_colmd9_row);
+                    divPanelBody_row_colmd12_colmd9_row.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd3);
+                    divPanelBody_row_colmd12_colmd9_row.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd9);
+                    divPanelBody_row_colmd12_colmd9_row_colMd3.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd3_a);
+                    divPanelBody_row_colmd12_colmd9_row_colMd3_a.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd3_a_img);
+                    divPanelBody_row_colmd12_colmd9_row_colMd9.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd9_div);
+                    divPanelBody_row_colmd12_colmd9_row_colMd9_div.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd9_div_span);
+                    divPanelBody_row_colmd12_colmd9_row_colMd9.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd9_br);
+                    divPanelBody_row_colmd12_colmd9_row_colMd9.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd9_div2);
+                    divPanelBody_row_colmd12_colmd9_row_colMd9.appendChild(divPanelBody_row_colmd12_colmd9_row_colMd9_div2_button);
+
+                }
+           }//end for
 
              // var id_pedido="";
              // var div=document.getElementById('TusPedidos');
@@ -61,3 +163,21 @@ $(document).ready(function () {
     
 
 });//end document.ready
+
+function crearElementos(element, attribute, inner){
+    if(typeof(element) === "undefined"){
+        return false;}
+    if(typeof(inner) === "undefined"){
+        inner = "";}
+    var el = document.createElement(element);
+    if(typeof(attribute) === 'object'){
+        for(var key in attribute){
+            el.setAttribute(key,attribute[key]);}
+        }
+        if(!Array.isArray(inner)){inner = [inner];}
+        for(var k = 0;k < inner.length;k++){
+            if(inner[k].tagName){el.appendChild(inner[k]);}
+            else{el.appendChild(document.createTextNode(inner[k]));}
+        }
+    return el;
+}
